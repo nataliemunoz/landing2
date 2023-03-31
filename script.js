3e45fbb7ea8cf305abf65692b48de796b6e64a8b
Natalie Muñoz <>
2023-03-31 23:04:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function saludo(){
+  alert("hola mundo");
+  var nombre = prompt("Ingrese su nombre: ");
+  alert("hola " + nombre);
+}
+
+var inicio = document.getElementsByClassName("link")[0];
+
+inicio.addEventListener("click", function() {
+  saludo();
+});
+
+var acercade = document.getElementsByClassName("link")[1];
+
+acercade.addEventListener("click", function() {
+  alert("gracias por visitar la página");
+});
